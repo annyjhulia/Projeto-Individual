f80d08b3eda4b54590f5fbfff7602228a304b490
Anny <anny.cerazi@sou.inteli.edu.br>
2025-06-15 22:33:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 const userRoutes = require("./routes/allRoutes");
 const pool = require("./config/database.js");
 
@@ -12,7 +13,20 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
+// Set up EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// API routes
 app.use("/api", userRoutes);
+
+// Serve the home page
+app.get('/', (req, res) => {
+  res.render('pages/home');
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
